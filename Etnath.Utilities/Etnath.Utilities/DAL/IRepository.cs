e7fcd04e8594fa578b02aea1f580b31bad19a81f
Etnath <kevin.giffone@gmail.com>
2016-12-04 18:20:22 +0100

    From Greenround.Portable.Utilities

---
+++ Etnath.Utilities/Etnath.Utilities/DAL/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenRound.Portable.Utilities.DAL
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        IEnumerable<T> GetAll();
+        T FindById(int id);
+    }
+}
---
+++ Etnath.Utilities/Etnath.Utilities/DAL/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenRound.Portable.Utilities.DAL
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+        void Rollback();
+    }
+}
