44b1e05b25df6a43021403ca4c1dd03eab6d633e
Etnath <Kevin.giffone@gmail.com>
2016-12-06 22:06:23 +0100

    Unit of work first implementation (WIP)

--- Etnath.Utilities/Etnath.Utilities/DAL/UnitOfWork.cs
+++ Etnath.Utilities/Etnath.Utilities/DAL/UnitOfWork.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 
 namespace GreenRound.Portable.Utilities.DAL
 {
-    class UnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
+        private DbContext _dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Commit()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void Rollback()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
